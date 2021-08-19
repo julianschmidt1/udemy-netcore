9569064f6a5f9ad7243b3fb4af00a64ec7e24d28
julianschmidt <julian.schmidt@axiomait.com>
2021-08-19 17:16:55 -0300

    console exercices

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa tu nombre: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Ingresa una ciudad: ");
+            var city = Console.ReadLine();
+            Console.WriteLine("Hola " + name + " bienvenido a " + city);
+
+
+        }
+    }
+}
