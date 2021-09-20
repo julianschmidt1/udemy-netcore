b67f2e218ca644224c1561c3e2576d913bb7816b
julianschmidt <julian.schmidt@axiomait.com>
2021-09-20 18:35:01 -0300

    added exercies

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,13 +6,55 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ingresa tu nombre: ");
-            var name = Console.ReadLine();
-            Console.WriteLine("Ingresa una ciudad: ");
-            var city = Console.ReadLine();
-            Console.WriteLine("Hola " + name + " bienvenido a " + city);
+            //Console.WriteLine("Ingresa tu nombre: ");
+            //var name = Console.ReadLine();
+            //Console.WriteLine("Ingresa una ciudad: ");
+            //var city = Console.ReadLine();
+            //Console.WriteLine("Hola " + name + " bienvenido a " + city);
 
+            //1) Crear 3 variables numéricas con el valor que tu quieras y en otra variable
+            //numérica guardar el valor de la suma de las 3 anteriores. Mostrar por consola
+            //int num1 = 1;
+            //int num2 = 2;
+            //int num3 = 3;
+            //int result = num1 + num2 + num3;
+            //Console.WriteLine("La suma es: " + result);
 
+            //2 Pedir dos números al usuario por teclado y decir que número es el mayor.
+            //string firstInput = Console.ReadLine();
+            //string secondInput = Console.ReadLine();
+            //int num1 = Convert.ToInt32(firstInput);
+            //int num2 = Convert.ToInt32(secondInput);
+
+            //if (num1 > num2)
+            //{
+            //    Console.WriteLine("Num mayor: " + num1);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Num mayor: " + num2);
+            //}
+
+            // 1-100 for
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //// 1-100 pares
+            //int[] arr = new int[100];
+            //for (int i = 0; i < 99; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        arr[i] = i;
+            //        Console.WriteLine(arr[i]);
+            //    }
+            //}
+            //foreach (int item in arr)
+            //{
+            //    Console.WriteLine("a: " + item);
+            //}
         }
     }
 }
